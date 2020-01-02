5f86a1e22b09862a7b095808fc17c6068fae3dae
nh16 <l_hoang07@yahoo.com>
2020-01-02 14:11:45 +0700

    first

---
+++ src/main/java/com/mycompany/assignment3/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+import javax.swing.JFrame;
+import A3.*;
+
+
+public class Main
+{
+    
+    public static void main(String[] args)
+    {       
+        OuterJFrame outerJFrame = new OuterJFrame();
+        
+        outerJFrame.setTitle("Employee System");
+        outerJFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        outerJFrame.pack();
+        outerJFrame.setVisible(true);
+    }
+}
